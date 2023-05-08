664a2045c5201ab4a166d30133e58ab098ea7d35
Shiiq <anjunatown@gmail.com>
2023-05-08 21:44:35 +0300

    Houston, we have a problem

--- cli/input_parser.py
+++ cli/input_parser.py
@@ -1,6 +1,8 @@
 from common.constants import BankOperationsFromInput
 
-from common.database.models.dto_models import BankStatementDTO, DepositOrWithdrawDTO
+from common.database.models.dto_models import (BankStatementDTO,
+                                               DepositDTO,
+                                               WithdrawDTO)
 
 
 class InputParserService:
@@ -18,33 +20,37 @@ class InputParserService:
     def __set_output_data(self, output_data):
         self.__output_data = output_data
 
-    def parse_args(self, input_data: str) -> BankStatementDTO | DepositOrWithdrawDTO:
+    def parse_args(self, input_data: str) -> BankStatementDTO | DepositDTO | WithdrawDTO:
         operation, *args = input_data.strip().split()
         if operation.lower() == BankOperationsFromInput.BANK_STATEMENT:
-            bank_statement_dto = self._validate_bank_statement_operation(operation,
-                                                                    args)
+            bank_statement_dto = self._get_bank_statement_operation_dto(operation.lower(),
+                                                                        args)
             return bank_statement_dto
+
         elif operation.lower() in [BankOperationsFromInput.DEPOSIT,
                                    BankOperationsFromInput.WITHDRAW]:
-            deposit_withdraw_dto = self._validate_deposit_withdraw_operation(operation,
-                                                                             args)
+            deposit_withdraw_dto = self._get_deposit_withdraw_operation_dto(operation.lower(),
+                                                                            args)
             return deposit_withdraw_dto
 
 
-    def _validate_bank_statement_operation(self, operation: str, args: list) -> BankStatementDTO:
+    def _get_bank_statement_operation_dto(self, operation: str, args: list) -> BankStatementDTO:
         first_name, last_name, since, till = args
-        current_operation = BankStatementDTO(operation=operation.lower(),
-                                             first_name=first_name.lower(),
-                                             last_name=last_name.lower(),
-                                             since=since,
-                                             till=till)
-        return current_operation
+        return BankStatementDTO(operation=operation,
+                                first_name=first_name.lower(),
+                                last_name=last_name.lower(),
+                                since=since,
+                                till=till)
 
-
-    def _validate_deposit_withdraw_operation(self, operation: str, args: list) -> DepositOrWithdrawDTO:
+    def _get_deposit_withdraw_operation_dto(self, operation: str, args: list) -> DepositDTO | WithdrawDTO:
         first_name, last_name, amount = args
-        current_operation = DepositOrWithdrawDTO(operation=operation.lower(),
-                                                 first_name=first_name.lower(),
-                                                 last_name=last_name.lower(),
-                                                 amount=amount)
-        return current_operation
+        if operation == BankOperationsFromInput.DEPOSIT:
+            return DepositDTO(operation=operation,
+                              first_name=first_name.lower(),
+                              last_name=last_name.lower(),
+                              amount=amount)
+        elif operation == BankOperationsFromInput.WITHDRAW:
+            return WithdrawDTO(operation=operation,
+                               first_name=first_name.lower(),
+                               last_name=last_name.lower(),
+                               amount=amount)

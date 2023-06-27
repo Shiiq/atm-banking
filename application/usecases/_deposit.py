17ed10c88a168a565fa25faaa9a41bceb6132432
Shiiq <anjunatown@gmail.com>
2023-06-27 20:30:01 +0300

    A quick patch to keep the boss happy

--- application/usecases/_deposit.py
+++ application/usecases/_deposit.py
@@ -14,7 +14,10 @@ class _Deposit:
         self._customer_service = CustomerService(uow=uow)
         self._operation_service = OperationService(uow=uow)
 
-    async def __call__(self, input_data: dto.DepositInput):
+    async def __call__(
+            self,
+            input_data: dto.DepositInput
+    ):
         # input_data = dto.DepositInput(
         #     customer=dto.CustomerInput(first_name="jaks",
         #                                last_name="korspe"),
@@ -23,38 +26,29 @@ class _Deposit:
         # )
         async with self.uow:
 
-            # get or register customer
             try:
-                customer_search_data = dto.BankCustomerSearch(
-                    first_name=input_data.customer.first_name,
-                    last_name=input_data.customer.last_name
-                )
+                customer_search_data = dto.BankCustomerSearch(first_name=input_data.customer.first_name,
+                                                              last_name=input_data.customer.last_name)
+                customer = await self._customer_service.by_fullname(customer_search_data=customer_search_data)
 
-                customer = await self._customer_service.by_fullname(
-                    customer_search_data=customer_search_data
-                )
-
-            except CustomerNotExist(
-                    first_name=input_data.customer.first_name,
-                    last_name=input_data.customer.last_name
-            ) as err:
+            except CustomerNotExist(first_name=input_data.customer.first_name,
+                                    last_name=input_data.customer.last_name) as err:
                 # logging err
+                customer_create_data = dto.BankCustomerCreate(first_name=input_data.customer.first_name,
+                                                              last_name=input_data.customer.last_name)
+                customer = await self._customer_service.create(customer_create_data=customer_create_data)
+                # logging creation
 
-                customer_create_data = dto.BankCustomerCreate(
-                    first_name=input_data.customer.first_name,
-                    last_name=input_data.customer.last_name
-                )
+            account_search_data = dto.BankAccountSearch(id=customer.bank_account_id)
+            current_account = await self._account_service.by_id(account_search_data=account_search_data)
+            account_update_data = dto.BankAccountUpdate(id=current_account.id,
+                                                        balance=(current_account.balance + input_data.operation.amount))
 
-                customer = await self._customer_service.create(
-                    customer_create_data=customer_create_data
-                )
 
-            account_search_data = dto.BankAccountSearch(
-                id=customer.bank_account_id
-            )
-            account = self._account_service.by_id(
-                account_search_data=account_search_data
-            )
-            pass
-        pass
-
+    def update_current_balance_data(
+            self,
+            account_search_data: dto.BankAccountSearch,
+            operation_data: dto.OperationInput
+    ) -> dto.BankAccountRead:
+        updated_balance = account.balance + deposit_value
+        return updated_balance

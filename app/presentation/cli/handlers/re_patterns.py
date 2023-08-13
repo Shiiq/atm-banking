eb7e7d743b43e88967e610bc215955b5d2783040
Shiiq <anjunatown@gmail.com>
2023-08-13 21:42:12 +0300

    Sometimes I believe compiler ignores all my comments

---
+++ app/presentation/cli/handlers/re_patterns.py
@@ -0,0 +1,30 @@
+
+
+OPERATION_PATTERN = r"bank[\s_]?statement|deposit|withdraw|exit"
+
+BANK_STATEMENT_PATTERN = (
+    r"(?P<operation_type>bank[\s_]?statement)\s+"
+    r"(?P<first_name>[^\W\d]+)\s+"
+    r"(?P<last_name>[^\W\d]+)\s+"
+    r"(?P<since>(\d{4})[.-](\d{2})[.-](\d{2}))\s+"
+    r"(?P<till>(\d{4})[.-](\d{2})[.-](\d{2}))"
+)
+
+DEPOSIT_PATTERN = (
+    r"(?P<operation_type>deposit)\s+"
+    r"(?P<first_name>[^\W\d]+)\s+"
+    r"(?P<last_name>[^\W\d]+)\s+"
+    r"(?P<amount>\d{1,7})"
+)
+
+WITHDRAW_PATTERN = (
+    r"(?P<operation_type>withdraw)\s+"
+    r"(?P<first_name>[^\W\d]+)\s+"
+    r"(?P<last_name>[^\W\d]+)\s+"
+    r"(?P<amount>\d{1,7})"
+)
+
+BANK_STATEMENT_VARIATIONS_PATTERN = r"bank[\s_]?statement"
+REPL_BANK_STATEMENT_PATTERN = r"bank_statement"
+DATE_VARIATIONS_PATTERN = r"(\d{4})[.-](\d{2})[.-](\d{2})"
+REPL_DATE_PATTERN = r"\1-\2-\3"
--- app/presentation/cli/handlers/regexxs.py
+++ app/presentation/cli/handlers/regexxs.py
@@ -1,36 +1,31 @@
-from enum import StrEnum
 import re
 
-from app.application.dto import BankStatementInput, DepositInput, WithdrawInput, BankOperationsFromInput
+from app.application.dto import (BankStatementInput,
+                                 DepositInput,
+                                 WithdrawInput,
+                                 BankOperationsFromInput)
+from app.presentation.cli.handlers.re_patterns import (OPERATION_PATTERN,
+                                                       BANK_STATEMENT_PATTERN,
+                                                       DEPOSIT_PATTERN,
+                                                       WITHDRAW_PATTERN)
 
-operations_pattern = r"bank[\s_]+statement|deposit|withdraw"
-bank_statement_args_pattern = (
-    r"(?P<operation_type>bank[\s_]?statement|deposit|withdraw)\s+"
-    r"(?P<first_name>[^\W\d]+)\s+"
-    r"(?P<last_name>[^\W\d]+)\s+"
-    r"(?P<since>\d{4}[.-]\d{2}[.-]\d{2})\s+"
-    r"(?P<till>\d{4}[.-]\d{2}[.-]\d{2})"
-)
-deposit_or_withdraw_args_pattern = (
-    r"(?P<operation_type>bank[\s_]+statement|deposit|withdraw)\s+"
-    r"(?P<first_name>[^\W\d]+)\s+"
-    r"(?P<last_name>[^\W\d]+)\s+"
-    r"(?P<amount>\d{1,7})"
-)
+
 operation_provider = {
-    "deposit": {"pattern": deposit_or_withdraw_args_pattern,
-                "parsed_data_model": BankOperationsFromInput.DEPOSIT},
-    "withdraw": {"pattern": deposit_or_withdraw_args_pattern,
-                 "parsed_data_model": BankOperationsFromInput.WITHDRAW},
-    "bank statement": {"pattern": bank_statement_args_pattern,
-                       "parsed_data_model": BankOperationsFromInput.BANK_STATEMENT},
-    "bank_statement": {"pattern": bank_statement_args_pattern,
-                       "parsed_data_model": BankOperationsFromInput.BANK_STATEMENT},
-    "bankstatement": {"pattern": bank_statement_args_pattern,
-                      "parsed_data_model": BankOperationsFromInput.BANK_STATEMENT},
+    # "operation type from cli": {"pattern for args",
+    #                             "parsed data model"},
+    "bank statement": {"args_pattern": BANK_STATEMENT_PATTERN,
+                       "parsed_data_model": BankStatementInput},
+    "bank_statement": {"args_pattern": BANK_STATEMENT_PATTERN,
+                       "parsed_data_model": BankStatementInput},
+    "bankstatement": {"args_pattern": BANK_STATEMENT_PATTERN,
+                      "parsed_data_model": BankStatementInput},
+    "deposit": {"args_pattern": DEPOSIT_PATTERN,
+                "parsed_data_model": DepositInput},
+    "withdraw": {"args_pattern": WITHDRAW_PATTERN,
+                 "parsed_data_model": WithdrawInput},
 }
 
-operations = [
+operations_input = [
     input_bank_statement1 := " bank statement   ",
     # input_bank_statement2 := "  banKSTateMent ",
     input_bank_statement3 := "bank_statement   ",
@@ -38,24 +33,20 @@ operations = [
     input_deposit := "   dEpoSit   ",
     input_withdraw := "   withdrAw ",
 ]
-deposit_withdraw_and_args = [
-    deposit_and_args := "   dEpoSit   lolo  vosk  2050 z",
-    withdraw_and_args := " withdrAw  posa  keow  104010",
+deposit_withdraw_and_args_input = [
+    deposit_and_args := "   dEposit   lolo  vosk  2050 z",
+    withdraw_and_args := " withdraw  posa  keow  104010",
 ]
-bank_statement_and_args = [
+bank_statement_and_args_input = [
     bank_statement_and_args1 := " bank statement  lolo   vosk  2016-07-12 2019.03-21",
-    bank_statement_and_args2 := "  banKSTateMent  lolo dvosk  2020-04-12    2005.05.01",
+    bank_statement_and_args2 := "  bankstatement  lolo dvosk  2020-04-12    2005.05.01",
     bank_statement_and_args3 := "bank_statement lolo   vosk  2014.12.01   2015-12-06",
 ]
-
-def check_operation_type(pattern, raw_data):
-    decomposed_input = re.search(pattern, raw_data, flags=re.I)
-    operation_type = decomposed_input.group().lower()
+all_in_one_input = [*bank_statement_and_args_input, *deposit_withdraw_and_args_input]
 
 
 def parsing_deposit_withdraw_input(pattern, raw_data, output_model):
     decomposed_input = re.search(pattern, raw_data, flags=re.I)
-    operation_type = decomposed_input.group("operation_type")
     first_name = decomposed_input.group("first_name")
     last_name = decomposed_input.group("last_name")
     amount = decomposed_input.group("amount")
@@ -79,5 +70,25 @@ def parsing_bank_statement_input(pattern, raw_data, output_model):
                         till=till)
 
 
-for i in bank_statement_and_args:
-    print(parsing_bank_statement_input(bank_statement_args_pattern, i))
+def check_operation_type(pattern, raw_data):
+    decomposed_input = re.search(pattern, raw_data, flags=re.I)
+    if not decomposed_input:
+        raise Exception("WRONG OPERATION")
+    operation_type = decomposed_input.group().lower()
+    operation_type = re.sub(r"bank[\s_]?statement", r"bank_statement", operation_type)
+    # print(operation_type, type(operation_type))
+    pattern = operation_provider.get(operation_type).get("args_pattern")
+    output_model = operation_provider.get(operation_type).get("parsed_data_model")
+    if operation_type == BankOperationsFromInput.BANK_STATEMENT:
+        return parsing_bank_statement_input(pattern=pattern,
+                                            raw_data=raw_data,
+                                            output_model=output_model)
+    elif operation_type == BankOperationsFromInput.DEPOSIT or BankOperationsFromInput.WITHDRAW:
+        return parsing_deposit_withdraw_input(pattern=pattern,
+                                              raw_data=raw_data,
+                                              output_model=output_model)
+
+from pprint import pprint
+
+for i in all_in_one_input:
+    pprint(check_operation_type(OPERATION_PATTERN, i).model_dump_json())

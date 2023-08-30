241af2d08cbe69c77ad32cd743fa63b835a1e81d
Shiiq <anjunatown@gmail.com>
2023-08-30 23:59:06 +0300

    yuck

--- __main__.py
+++ __main__.py
@@ -7,11 +7,17 @@ from app.infrastructure.logger.builder import build_main_logger
 from app.presentation.api_runner import api_start
 from app.presentation.cli_runner import cli_start
 
-log_config = get_log_config()
-logger = build_main_logger(log_config=log_config)
+
 
 
 async def parse_args():
+
+    log_config = get_log_config()
+    build_main_logger(log_config=log_config)
+    # logg = logging.getLogger(__name__)
+    # logging.basicConfig(level=logging.INFO,
+    #                     format="%(asctime)s :: %(levelname)s :: %(name)s :: %(message)s")
+
     main_parser = argparse.ArgumentParser(description="api or cli")
 
     group = main_parser.add_mutually_exclusive_group(required=True)
@@ -22,8 +28,10 @@ async def parse_args():
     args = main_parser.parse_args()
 
     if args.api:
+        # logg.info("running the api application")
         await api_start()
     elif args.cli:
+        # logg.info("running the cli application")
         await cli_start()
 
 
--- app/infrastructure/config/log_config.py
+++ app/infrastructure/config/log_config.py
@@ -1,4 +1,9 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings
+
+_BASE_DIR = Path(__file__).parent.parent.parent.parent
+_LOGS_PATH = _BASE_DIR.joinpath("logs/", "operations_.txt")
 
 
 class LogConfig(BaseSettings):
@@ -7,10 +12,11 @@ class LogConfig(BaseSettings):
     LOG_FORMAT: str = "%(asctime)s :: %(levelname)s :: %(name)s :: %(message)s"
     DATE_FORMAT: str = "%Y-%m-%d %H:%M:%S"
     NAME: str = "atm-banking.main"
-    STREAM_LOG_LEVEL: str = "ERROR"
+    STREAM_LOG_LEVEL: str = "INFO"
     BASE_LEVEL: str = "INFO"
     FILE_LOG_LEVEL: str = "INFO"
-    FILE_LOG_PATH: str = "./logs/operations.txt"
+    FILE_LOG_PATH: Path = _LOGS_PATH
+    FILE_BACKUP_COUNT: int = 3
 
 
 def get_log_config():
